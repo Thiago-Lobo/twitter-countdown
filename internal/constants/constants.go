26110e0689fbeb340c7ff6d39961a756899d3df3
Thiago Lobo <thiagolobo@poli.ufrj.br>
2022-06-28 00:02:55 -0300

    Adds twitter reply settings enum + translators

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -9,3 +9,38 @@ const OAUTH_TOKEN_SECRET = "GOTWI_OAUTH_TOKEN_SECRET"
 const END_DATE = "END_DATE"
 const TWEET_TEMPLATE = "TWEET_TEMPLATE"
 const POST_TIME = "POST_TIME"
+
+const REPLY_SETTING = "REPLY_SETTING"
+
+// Tweet reply settings
+
+type TweetReplySetting int
+
+const (
+	NONE TweetReplySetting = iota
+	FOLLOWING
+	MENTIONED
+)
+
+var tweetReplySettingStrings = [...]string{"", "following", "mentionedUsers"}
+
+func (setting TweetReplySetting) String() *string {
+	if setting == NONE {
+		return nil
+	}
+
+	return &tweetReplySettingStrings[setting]
+}
+
+var (
+	tweetReplySettingMap = map[string]TweetReplySetting{
+		"":               NONE,
+		"following":      FOLLOWING,
+		"mentionedUsers": MENTIONED,
+	}
+)
+
+func StringToTweetReplySetting(str string) TweetReplySetting {
+	setting := tweetReplySettingMap[str]
+	return setting
+}
